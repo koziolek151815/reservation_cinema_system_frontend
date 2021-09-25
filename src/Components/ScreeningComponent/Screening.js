bcea4e4654e43100107acc4bfe9114080b24f3c4
koziolek151815 <koziolek151815x@gmail.com>
2021-09-25 14:51:01 +0200

    image displaying and adding flex to screening start and button

--- src/Components/ScreeningComponent/Screening.js
+++ src/Components/ScreeningComponent/Screening.js
@@ -7,19 +7,26 @@ import Button from "react-bootstrap/cjs/Button";
 
 
 function Screening(props) {
-    const goToTicketSelection = (screeningId) => {
-        window.location.replace(`/screening/${screeningId}`);
-    }
+    const [image, setImage] = useState("");
+    useEffect(() => {
+        const fetchData = async () => {
+            const result = await axios(
+                process.env.REACT_APP_BACKEND_URL + `/movies/getPhoto?movieId=${props.screening.movie.movieId}`, {headers: {"Authorization": `Bearer ${localStorage.getItem('token')}`}}
+            );
+            setImage(result.data);
+        };
+        fetchData();
+    }, []);
     return (
-        <div className="Screening">
-            <div>
+        <div className="m-auto">
+            <div className="m-auto">
                 <p> {props.screening.movie.title}</p>
+                {image &&<img alt="post img" src={"data:image/jpeg;base64,"+image}/>}
                 {props.screening.screenings.map(screening =>
-                    <div>
-                        <p> {formatDate(screening.startScreening)}</p>
-                        {/*<a className="btn btn-default bg-success"*/}
-                        {/*   href={`/screening/${screening.screeningId}`}>Zarezerwuj bilet</a>*/}
-                        <Button onClick={()=> props.history.push(`/screening/${screening.screeningId}`)}>Zarezerwuj bilet</Button>
+                    <div className="d-flex m-auto align-items-center justify-content-center">
+                        <p> {formatDate(screening.startScreening).slice(1).slice(-5)}</p>
+                        <Button onClick={() => props.history.push(`/screening/${screening.screeningId}`)}>Zarezerwuj
+                            bilet</Button>
                     </div>
                 )}
             </div>
