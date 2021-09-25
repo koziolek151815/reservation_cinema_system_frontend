fd75a923c98becaa78bf1416d109fbc7befd4ace
koziolek151815 <koziolek151815x@gmail.com>
2021-09-25 15:03:19 +0200

    quick styling

--- src/Components/ScreeningComponent/Screening.js
+++ src/Components/ScreeningComponent/Screening.js
@@ -19,12 +19,12 @@ function Screening(props) {
     }, []);
     return (
         <div className="m-auto">
-            <div className="m-auto">
+            <div className="m-auto mb-4">
                 <p> {props.screening.movie.title}</p>
-                {image &&<img alt="post img" src={"data:image/jpeg;base64,"+image}/>}
+                {image && <img className="mb-3" alt="post img" src={"data:image/jpeg;base64," + image}/>}
                 {props.screening.screenings.map(screening =>
                     <div className="d-flex m-auto align-items-center justify-content-center">
-                        <p> {formatDate(screening.startScreening).slice(1).slice(-5)}</p>
+                        <p className="p-1"> {formatDate(screening.startScreening).slice(1).slice(-5)}</p>
                         <Button onClick={() => props.history.push(`/screening/${screening.screeningId}`)}>Zarezerwuj
                             bilet</Button>
                     </div>
